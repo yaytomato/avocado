216f9cf80f1c61037e80dd5c8e4ef6b6d39a29d5
yaytomato <yehoon_s@naver.com>
2020-11-20 02:34:37 +0900

    Implement SearchBar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useState } from "react";
+import { Formik, Form, Field } from "formik";
+
+import { searchBar as contents } from "../constants/home";
 
 const SearchBar: React.FunctionComponent = () => {
-  return <div>search bar</div>;
+  const [platform, setPlatform] = useState<string>("naver");
+
+  const PlatformBtn = ({ id, children }) => (
+    <p
+      className={`cursor-pointer ${
+        id === platform ? "font-black" : "font-gray"
+      }`}
+      onClick={() => setPlatform(id)}
+    >
+      {children}
+    </p>
+  );
+
+  const onSearch = ({ keyword }) => {
+    window.location.href = contents[platform] + keyword;
+  };
+
+  return (
+    <React.Fragment>
+      <div className="flex">
+        <PlatformBtn id="naver">네이버</PlatformBtn>
+        <PlatformBtn id="google">구글</PlatformBtn>
+      </div>
+
+      <Formik
+        initialValues={{
+          keyword: "",
+        }}
+        onSubmit={onSearch}
+      >
+        {() => (
+          <Form>
+            <Field type="text" name="keyword" />
+          </Form>
+        )}
+      </Formik>
+    </React.Fragment>
+  );
 };
 
 export default SearchBar;
--- constants/home.ts
+++ constants/home.ts
@@ -7,3 +7,8 @@ export const navBar = {
   updateMsg: " 업데이트됨",
   toArchive: "지난 건강들",
 };
+
+export const searchBar = {
+  naver: "https://search.naver.com/search.naver?query=",
+  google: "https://www.google.com/search?q=",
+};
