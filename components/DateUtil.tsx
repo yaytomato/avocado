66ea14cc8f6a60b24dccc7d418afdcb02791fd3a
yaytomato <yehoon_s@naver.com>
2020-11-25 00:13:29 +0900

    Implement DateUtil

--- components/DateUtil.tsx
+++ components/DateUtil.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import moment from "moment";
+import "moment/locale/ko";
+
+import { monthInKor, getWeekNumInKor } from "../helper";
 
 const DateUtil: React.FunctionComponent = () => {
-  return <div>date</div>;
+  const now = moment();
+
+  return (
+    <div>
+      {monthInKor[now.month()]} {getWeekNumInKor(now)} {now.format("dddd")}
+    </div>
+  );
 };
 
 export default DateUtil;
--- helper.ts
+++ helper.ts
@@ -9,3 +9,44 @@ export const getLatestReleaseDate = (): string => {
     ? today.format("YYYY-MM-DD")
     : yesterday.format("YYYY-MM-DD");
 };
+
+export const monthInKor = [
+  "일월",
+  "이월",
+  "삼월",
+  "사월",
+  "오월",
+  "유월",
+  "칠월",
+  "팔월",
+  "구월",
+  "시월",
+  "십일월",
+  "십이월",
+];
+
+const weekNumInKor = [
+  "첫번째",
+  "두번째",
+  "세번째",
+  "네번째",
+  "다섯번째",
+  "마지막",
+];
+
+export const getWeekNumInKor = (date: moment.Moment): string => {
+  let firstWeekdayOfMonth = moment().startOf("month").day(date.day());
+  if (firstWeekdayOfMonth.month() < date.month())
+    firstWeekdayOfMonth = firstWeekdayOfMonth.add(1, "week");
+
+  let weekNum = date.diff(firstWeekdayOfMonth, "weeks");
+
+  let lastWeekdayOfMonth = moment().endOf("month").day(date.day());
+  if (lastWeekdayOfMonth.month() > date.month())
+    lastWeekdayOfMonth = lastWeekdayOfMonth.subtract(1, "week");
+
+  if (date.date() === lastWeekdayOfMonth.date())
+    weekNum = weekNumInKor.length - 1;
+
+  return weekNumInKor[weekNum];
+};
