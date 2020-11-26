dc8665f96d462d566955963b50fa484212df56b2
yaytomato <yehoon_s@naver.com>
2020-11-27 01:04:45 +0900

    Ask permission for geolocation + Retrieve latitude, longitude from it

--- components/WeatherUtil.tsx
+++ components/WeatherUtil.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const WeatherUtil: React.FunctionComponent = () => {
-  return <div>weather</div>;
+  const [temp, setTemp] = useState("");
+
+  const fetchWeather = (position) => {
+    const { latitude, longitude } = position.coords;
+    setTemp(latitude);
+  };
+
+  if (window.navigator.geolocation) {
+    window.navigator.geolocation.getCurrentPosition(fetchWeather, console.log);
+  }
+
+  return <div>{temp}</div>;
 };
 
 export default WeatherUtil;
