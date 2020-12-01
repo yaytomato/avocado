54eacc5c1d156b5123ec59735cbefdd75c0e3202
yaytomato <yehoon_s@naver.com>
2020-12-01 20:44:50 +0900

    Update .gitignore + Update contents in Loading

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -6,7 +6,9 @@ const Loading: React.FC = () => (
   <div className="flex">
     <img src={contents.logo} alt="아보카도 로고" />
     <div>
-      <p>{contents.subheading}</p>
+      <p>
+        {contents.subheading} <span className="font-bold">아보카도</span>
+      </p>
       <p>{contents.heading}</p>
     </div>
   </div>
